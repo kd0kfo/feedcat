fc6c85fd00249605c7611590e94019f7eee52b61
David Coss <david@davecoss.com>
2014-03-16 10:12:23 -0500

    Added serial id for List

--- src/com/davecoss/tomcat/feedcat/List.java
+++ src/com/davecoss/tomcat/feedcat/List.java
@@ -12,6 +12,8 @@ import javax.servlet.*;
 
 public class List extends HttpServlet {
 	
+	private static final long serialVersionUID = 1L;
+
 	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
